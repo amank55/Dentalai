5037ddefdd3ffae3877ff474aaa413e506abccf4
Aman Kumar <amankumarr2005ak@gmail.com>
2026-01-20 21:21:08 +0530

    Refactor code structure for improved readability and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 import { Button } from "@/components/ui/button"
+import {  SignInButton, SignOutButton } from "@clerk/nextjs"
 
 const page = () => {
   return (
-   <Button>Click me</Button>
+    <>
+    <div>
+       <SignOutButton>
+       <h1>SignedIn</h1> 
+      </SignOutButton>
+      <SignInButton mode="modal">
+        <h1>Lgout</h1>
+      </SignInButton>
+    </div>
+    </>
   )
 }
 export default page
