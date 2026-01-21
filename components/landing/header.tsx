04de59a11a630bfa170001e92bc92a43a8abdca9
Aman Kumar <amankumarr2005ak@gmail.com>
2026-01-21 17:22:49 +0530

    Implement header component structure with navigation

--- components/landing/header.tsx
+++ components/landing/header.tsx
@@ -2,3 +2,14 @@ import { SignInButton, SignUpButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../ui/button";
+
+function header (){
+return (
+    <nav className="fixed top-0">
+        <div>
+
+        </div>
+    </nav>
+)
+}
+export default header
