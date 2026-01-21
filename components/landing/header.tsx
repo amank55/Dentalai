01c591c3bf173976ee5395d73f401e6d530b1149
Aman Kumar <amankumarr2005ak@gmail.com>
2026-01-21 17:24:19 +0530

    Enhance header component styling for improved layout and positioning

--- components/landing/header.tsx
+++ components/landing/header.tsx
@@ -5,7 +5,7 @@ import { Button } from "../ui/button";
 
 function header (){
 return (
-    <nav className="fixed top-0">
+    <nav className="fixed top-0 right-0 left-0 z-50 px-6">
         <div>
 
         </div>
