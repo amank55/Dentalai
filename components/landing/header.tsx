97a64da3ddd51f6af5a7c46d9d97daed82c61c66
Aman Kumar <amankumarr2005ak@gmail.com>
2026-01-21 17:25:26 +0530

    Fix duplicate header function definition and clean up imports

--- components/landing/header.tsx
+++ components/landing/header.tsx
@@ -9,6 +9,17 @@ return (
         <div>
 
         </div>
+    </nav>import { SignInButton, SignUpButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+function header (){
+return (
+    <nav className="fixed top-0 right-0 left-0 z-50 px-6">
+        <div>
+
+        </div>
     </nav>
 )
 }
